731f8770146f3538ca169e01dacbe2476b4c8d77
Matt <mattchavez815@gmail.com>
2025-09-19 21:48:09 -0400

    fixing error 1

--- app/(Kambaz)/Courses/[cid]/Assignments/page.tsx
+++ app/(Kambaz)/Courses/[cid]/Assignments/page.tsx
@@ -1,67 +1,73 @@
+'use client';
+import Link from "next/link";
+
 export default function Assignments() {
   return (
     <div id="wd-assignments">
-      <input placeholder="Search for Assignments"
-             id="wd-search-assignment" />
+      <input
+        placeholder="Search for Assignments"
+        id="wd-search-assignment"
+      />
       <button id="wd-add-assignment-group">+ Group</button>
       <button id="wd-add-assignment">+ Assignment</button>
+
       <h3 id="wd-assignments-title">
-        ASSIGNMENTS 40% of Total <button>+</button> </h3>
+        ASSIGNMENTS 40% of Total <button>+</button>
+      </h3>
       <ul id="wd-assignment-list">
         <li className="wd-assignment-list-item">
-          <a href="/Courses/1234/Assignments/123"
-             className="wd-assignment-link" >
+          <Link href="/Courses/1234/Assignments/123" className="wd-assignment-link">
             A1 - ENV + HTML
-          </a> 
-          </li>
+          </Link>
+        </li>
         <li className="wd-assignment-list-item">
-          <a href="/Courses/1234/Assignments/123"
-             className="wd-assignment-link" >
+          <Link href="/Courses/1234/Assignments/123" className="wd-assignment-link">
             A2 - STARTING CSS
-          </a>
+          </Link>
         </li>
         <li className="wd-assignment-list-item">
-          <a href="/Courses/1234/Assignments/123"
-             className="wd-assignment-link" >
+          <Link href="/Courses/1234/Assignments/123" className="wd-assignment-link">
             A3 - FORMATTING PAGES
-          </a>
+          </Link>
         </li>
       </ul>
+
       <h3 id="wd-assignments-title">
-        QUIZZES 10% of Total <button>+</button> </h3>
+        QUIZZES 10% of Total <button>+</button>
+      </h3>
       <ul id="wd-assignment-list">
         <li className="wd-assignment-list-item">
-          <a href="/Courses/1234/Assignments/123"
-             className="wd-assignment-link" >
+          <Link href="/Courses/1234/Assignments/123" className="wd-assignment-link">
             Q1 - LECTURES 1-2
-          </a>
+          </Link>
         </li>
       </ul>
+
       <h3 id="wd-assignments-title">
-        EXAM 20% of Total <button>+</button> </h3>
+        EXAM 20% of Total <button>+</button>
+      </h3>
       <ul id="wd-assignment-list">
         <li className="wd-assignment-list-item">
-          <a href="/Courses/1234/Assignments/123"
-             className="wd-assignment-link" >
+          <Link href="/Courses/1234/Assignments/123" className="wd-assignment-link">
             E1 - MIDTERM
-          </a>
+          </Link>
         </li>
         <li className="wd-assignment-list-item">
-          <a href="/Courses/1234/Assignments/123"
-             className="wd-assignment-link" >
+          <Link href="/Courses/1234/Assignments/123" className="wd-assignment-link">
             E2 - FINAL
-          </a>
+          </Link>
         </li>
       </ul>
+
       <h3 id="wd-assignments-title">
-        PROJECTS 30% of Total <button>+</button> </h3>
+        PROJECTS 30% of Total <button>+</button>
+      </h3>
       <ul id="wd-assignment-list">
         <li className="wd-assignment-list-item">
-          <a href="/Courses/1234/Assignments/123"
-             className="wd-assignment-link" >
+          <Link href="/Courses/1234/Assignments/123" className="wd-assignment-link">
             P1- FINAL PROJECT
-          </a>
+          </Link>
         </li>
       </ul>
     </div>
-);}
+  ); }
