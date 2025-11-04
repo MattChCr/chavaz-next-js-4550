09b1e96e1e82a489c3997052f71257ecc9fedd70
Matt <mattchavez815@gmail.com>
2025-11-04 02:50:29 -0500

    fixing deployment 10

--- app/(Kambaz)/Account/Navigation.tsx
+++ app/(Kambaz)/Account/Navigation.tsx
@@ -1,16 +1,32 @@
+'use client';
+export const dynamic = 'force-dynamic';
+
+
 import Link from "next/link";
 import { useSelector } from "react-redux";
 import { RootState } from "../store";
+
 export default function AccountNavigation() {
   const { currentUser } = useSelector((state: RootState) => state.accountReducer);
-  const links = currentUser ? ["Profile"] : ["Signin", "Signup"];
- return (
-   <div id="wd-account-navigation" className="wd list-group fs-5 rounded-0">
-    <Link href="Signin" id="wd-account-navigation-link"
-        className="list-group-item active text-dark border-0"> Signin </Link>
-        <Link href="Signup" id="wd-account-navigation-link"
-        className="list-group-item text-danger border-0"> Signup </Link>
-        <Link href="Profile" id="wd-account-navigation-link"
-        className="list-group-item text-danger border-0"> Profile </Link>
-   </div>
-);}
+
+  const links = currentUser
+    ? [{ name: "Profile", href: "/Account/Profile" }]
+    : [
+        { name: "Signin", href: "/Account/Signin" },
+        { name: "Signup", href: "/Account/Signup" },
+      ];
+
+  return (
+    <div id="wd-account-navigation" className="wd list-group fs-5 rounded-0">
+      {links.map((link) => (
+        <Link
+          key={link.name}
+          href={link.href}
+          className="list-group-item text-dark border-0"
+        >
+          {link.name}
+        </Link>
+      ))}
+    </div>
+  );
+}
--- app/(Kambaz)/Account/layout.tsx
+++ app/(Kambaz)/Account/layout.tsx
@@ -1,17 +1,21 @@
+'use client'; 
+export const dynamic = 'force-dynamic';
+
 import { ReactNode } from "react";
 import AccountNavigation from "./Navigation";
-export default function AccountLayout({ children }:
-   Readonly<{ children: ReactNode }>) {
- return (
-   <div>
-    <h2> Account </h2>
-     <table>
-       <tbody>
-         <tr>
-           <td valign="top"><AccountNavigation /></td>
-           <td valign="top" width="100%">{children}</td>
-         </tr>
-       </tbody>
-     </table>
-  </div>
-);}
+
+export default function AccountLayout({ children }: Readonly<{ children: ReactNode }>) {
+  return (
+    <div>
+      <h2>Account</h2>
+      <div style={{ display: "flex", alignItems: "flex-start", gap: "1rem" }}>
+        <div style={{ flexShrink: 0 }}>
+          <AccountNavigation />
+        </div>
+        <div style={{ flexGrow: 1 }}>
+          {children}
+        </div>
+      </div>
+    </div>
+  );
+}
