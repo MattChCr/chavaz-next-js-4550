28d1ab6d4dd9daa9a0954588f329f7b632608bf5
Matt <mattchavez815@gmail.com>
2025-12-07 11:29:52 -0500

    a6 deployment issues 3

--- app/(Kambaz)/Account/client.ts
+++ app/(Kambaz)/Account/client.ts
@@ -17,23 +17,22 @@ export const findAllUsers = async () => {
 };
 
 export const findUsersByRole = async (role: string) => {
-  const response = await
-    axios.get(`${USERS_API}?role=${role}`);
+  const response = await axiosWithCredentials.get(`${USERS_API}?role=${role}`);
   return response.data;
 };
 
 export const findUsersByPartialName = async (name: string) => {
-  const response = await axios.get(`${USERS_API}?name=${name}`);
+  const response = await axiosWithCredentials.get(`${USERS_API}?name=${name}`);
   return response.data;
 };
 
 export const findUserById = async (id: string) => {
-  const response = await axios.get(`${USERS_API}/${id}`);
+  const response = await axiosWithCredentials.get(`${USERS_API}/${id}`);
   return response.data;
 };
 
 export const deleteUser = async (userId: string) => {
-  const response = await axios.delete( `${USERS_API}/${userId}` );
+  const response = await axiosWithCredentials.delete(`${USERS_API}/${userId}`);
   return response.data;
 };
 
@@ -43,7 +42,7 @@ export const updateUser = async (userId: string, user: Partial<User>) => {
 };
 
 export const createUser = async (user: Partial<User>) => {
-  const response = await axios.post(`${USERS_API}`, user);
+  const response = await axiosWithCredentials.post(`${USERS_API}`, user);
   return response.data;
 };
 
